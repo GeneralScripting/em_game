647906d92291f56e822ca61a7d4bf3c81411cc40
Maxmilian Woltersdorf <m.woltersdorf@hotmail.de>
2012-05-26 14:53:21 +0200

    country seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Team.create(:country => 'PL', :group => :a)
+Team.create(:country => 'GR', :group => :a)
+Team.create(:country => 'RU', :group => :a)
+Team.create(:country => 'CZ', :group => :a)
+Team.create(:country => 'NL', :group => :b)
+Team.create(:country => 'PT', :group => :b)
+Team.create(:country => 'DE', :group => :b)
+Team.create(:country => 'DK', :group => :b)
+Team.create(:country => 'ES', :group => :c)
+Team.create(:country => 'IT', :group => :c)
+Team.create(:country => 'IR', :group => :c)
+Team.create(:country => 'HR', :group => :c)
+Team.create(:country => 'FR', :group => :d)
+Team.create(:country => 'GB', :group => :d)
+Team.create(:country => 'UA', :group => :d)
+Team.create(:country => 'SE', :group => :d)
+
+
+
