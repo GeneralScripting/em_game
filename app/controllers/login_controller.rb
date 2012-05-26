4b71cdda83bacfd113e46beddb8f892f6b83b130
Maxmilian Woltersdorf <m.woltersdorf@hotmail.de>
2012-05-26 18:09:53 +0200

    games

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,6 +2,7 @@ class LoginController < ApplicationController
   
   def index
     @users = User.order('score DESC').limit(10)
+    @games = Game.order('DAY(start_at), start_at DESC').all
   end
   
 end
