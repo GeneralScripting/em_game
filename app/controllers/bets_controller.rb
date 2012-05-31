19789393de93047cc67cd8ffff8d6d9fc8cb2029
Maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2012-05-31 09:20:37 +0200

    wall posts

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -10,6 +10,13 @@ class BetsController < ApplicationController
   def create
     @bet = current_user.bets.new( params[:bet] )
     @bet.save
+    @game = Game.find( params[:bet][:game_id] )
+     if current_facebook_user
+          current_facebook_user.fetch
+          current_facebook_user.feed_create(
+            Mogli::Post.new(:message => "#{current_facebook_user.name} tippt: #{t(@game.team_a.country, :scope => 'countries')} #{@bet.team_a_goals} : #{@bet.team_b_goals} #{t(@game.team_b.country, :scope => 'countries')}")
+            )
+      end
     @game = @bet.game
     current_user.reload
   end
